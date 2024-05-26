92da977ab04a6ebacfe7efc231456025927ce968
Ludmila <ludmila27@hotmail.es>
2024-05-26 19:07:01 -0300

    Update ETipoHabitacion.java
    Modifica las opciones del enum ETipoHabitacion.

--- src/Enums/ETipoHabitacion.java
+++ src/Enums/ETipoHabitacion.java
@@ -1,5 +1,15 @@
 package Enums;
 
 public enum ETipoHabitacion {
-    CAMA_MATRIMONIAL,CAMAS_DOBLE,TRES_CAMAS,CUATRO_CAMAS,QUEEN_SIZE,QUEEN_SIZE_CAMASIMPLE;
+
+    SIMPLE(1,0),
+    DOBLE_TIPO_1(0,1), 
+    DOBLE_TIPO_2(2,0),
+    TRIPLE(1,1),
+    CUADRUPLE_TIPO_1(1,2),
+    CUADRUPLE_TIPO_2(0,2);
+
+
+    ETipoHabitacion(int cantidadCamasSimples, int cantidadCamasDobles) {
+    }
 }
