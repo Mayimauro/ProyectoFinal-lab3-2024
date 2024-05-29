9f7b52cdc61428c46626359a23fcea90d7e751dc
Blaseze <blaseze702@gmail.com>
2024-05-28 22:38:48 -0300

    Exceptions
    creacion de la exception de contraseña incorrecta

---
+++ src/Exceptions/ContraseniaIncorrectaException.java
@@ -0,0 +1,10 @@
+package Exceptions;
+
+import java.io.IOException;
+
+public class ContraseniaIncorrectaException extends IOException {
+    public ContraseniaIncorrectaException() {
+        super();
+    }
+    public void 
+}
