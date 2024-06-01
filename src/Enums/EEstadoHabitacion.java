4f54b4bab8b3ba2c5af6af4b8409bd4fce57e790
Ludmila <ludmila27@hotmail.es>
2024-05-31 22:42:45 -0300

    Create EEstadoHabitacion.java
    Crea el enum EEstadoHabitación para ayudar al control de la disponibilidad de la habitación.

---
+++ src/Enums/EEstadoHabitacion.java
@@ -0,0 +1,11 @@
+package Enums;
+
+/**
+ * Clase de tipo enum para indicar el estado actual de las habitaciones.
+ */
+public enum EEstadoHabitacion {
+    DISPONIBLE,
+    OCUPADA,
+    EN_LIMPIEZA,
+    EN_REPARACION;
+}
