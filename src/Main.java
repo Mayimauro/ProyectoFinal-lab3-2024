b2b2dcfb056f8cb23790bac19c61903083ea0832
Ludmila <ludmila27@hotmail.es>
2024-06-12 18:14:22 -0300

    Update Main.java
    borramos lo que no sirve

--- src/Main.java
+++ src/Main.java
@@ -12,46 +12,9 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws InicioSesionIncorrectoException {
 
-//        Domicilio domicilio = new Domicilio("a","1",false,"casa","bs","mar","1233");
-//        Persona persona = new Persona("Lud","i","123","lud@admin.com","1",domicilio);
-//        Domicilio domicilio2 = new Domicilio("b","1",false,"casa","bs","mar","1233");
-//        Persona persona2 = new Persona("bla","i","456","blas@conserjer.com","1",domicilio2);
-        Domicilio domicilio3 = new Domicilio("c","1",false,"casa","bs","mar","1233");
-        Persona persona3 = new Persona("May","i","789","mati@gmail.com","1",domicilio3);
-//
-//        List<Persona> personas = new ArrayList<>();
-//
-//        personas.add(persona);
-//        personas.add(persona2);
-//        personas.add(persona3);
-//
-//        guardarPersonasEnArchivo(personas,"Usuarios");
-
-
-//        List<Persona> personasTraidas = leerPersonasDesdeArchivo("prueba1");
-//
-//        for (Persona personas : personasTraidas) {
-//            System.out.println(personas);
-//        }
-
-//        persona.agregarAlArchivo(persona);
-//        persona2.agregarAlArchivo(persona2);
-//        persona3.agregarAlArchivo(persona3);
-
-
-
         Hotel hotel = new Hotel();
-        /**
-         * DESCOMENTAR PARA EJECUTAR LA INTERFAZ GRAFICA
-         */
+
         PantallaInicioSesion frame = new PantallaInicioSesion(hotel);
-
-
-        /**
-         * sexoanal
-         */
-
-
 
     }
 
